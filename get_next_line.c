4cb3f83ecbc6feb92dfe8a7d12a38eb466265ec4
Beatriz Chagas De Sousa <bchagas-@c1r2p1.42sp.org.br>
2025-08-30 07:52:53 -0300

    Comecando a GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 23:24:42 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/08/30 06:50:05 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/08/30 07:09:47 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,26 +42,23 @@ char	*get_next_line(int fd)
 }
 // #include <stdio.h>
 // #include <fcntl.h>
-// #include <unistd.h>
 
-// char *get_next_line(int fd);
-
-// int main()
+// int main(void)
 // {
-//     int fd = open("teste.txt", O_RDONLY);  // Abre o arquivo para leitura
+//     int fd;
+//     char *line;
+
+//     fd = open("teste.txt", O_RDONLY);
 //     if (fd == -1)
 //     {
 //         perror("Erro ao abrir o arquivo");
-//         return 1;
+//         return (1);
 //     }
-
-//     char *line;
-//     while ((line = get_next_line(fd)) != NULL)  // Chama a função até o EOF
+//     while ((line = get_next_line(fd)) != NULL)
 //     {
-//         printf("Linha: %s\n", line);
-//         free(line);  // Libera a memória alocada para a linha
+//         printf("%s\n", line);
+//         free(line);
 //     }
-
-//     close(fd);  // Fecha o arquivo
-//     return 0;
+//     close(fd);
+//     return (0);
 // }
